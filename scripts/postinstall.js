20159c9053f7d89f113244cf91f22f351b4ca206
Quinn Rohlf <qr@qrohlf.com>
2015-07-26 10:26:08 -0700

    Lint everything to JS standard.
    ...coming soon: webpack!

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,10 +1,11 @@
-var colors = require('colors');
+require('colors')
 
 try {
-  var canvas = require('canvas')
+  require('canvas')
+  console.log('\n\nTrianglify has been installed successfully with SVG, canvas, and PNG support.')
 } catch (e) {
-  console.log('\n\n\nTrianglify has been installed successfully with SVG support.\n'.green +
-    'For canvas support, you will need to install Cairo & node-canvas\n'.cyan +
+  console.log('\n\nTrianglify has been installed successfully with SVG support.\n'.yellow +
+    'For canvas and PNG support, you will need to install Cairo & node-canvas\n'.cyan +
     'On OSX, you can install node-canvas and Cairo with the following commands:`\n\n'.cyan +
     '    brew install cairo\n' +
     '    PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:/opt/X11/lib/pkgconfig npm install -S canvas\n\n' +
